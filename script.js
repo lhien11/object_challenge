5a26f7b6a43b4c3a0374f37fe944aa740c2ebcd6
Hien Le <hienle@tests-MacBook-Pro-2.local>
2016-10-26 18:09:48 -0500

    First Iteration

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+function State(st, rev, local){
+  this.state = st;
+  this.revenue = rev;
+  this.locationCount = local;
+  this.averageRevenue = function(){
+    var aveRev = rev / local;
+    return "$" + Math.round(aveRev);
+  }
+}
 // object instances
+var IL = new State ("IL", 5000, 12);
+var MN = new State ("MN", 300, 3);
+var NV = new State ("NV", 25000, 1);
 
 // push object instances to locations array
+locations.push(IL);
+locations.push(MN);
+locations.push(NV);
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
